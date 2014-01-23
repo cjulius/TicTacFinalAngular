04e2f341119afc10f27ee9ec557233f93f0038ff
CJulius <charlesejulius@gmail.com>
2014-01-22 18:59:40 -0800

    Added X and O Function

--- main.js
+++ main.js
@@ -33,9 +33,17 @@ function boardCtrl ($scope) {
 		$scope.winAry = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 		for (var i = 0; i < 8; i++) {
 			if ($scope.boxes[$scope.winAry[i][0]] == $scope.boxes[$scope.winAry[i][1]] && $scope.boxes[$scope.winAry[i][0]] == $scope.boxes[$scope.winAry[i][2]] && $scope.boxes[$scope.winAry[i][0]] !== "") {
-					document.getElementsByClassName("winner")[0].style.display="block";
-					$scope.winner = "You WIN!";
-					break;
+					if ($scope.xTurn == $scope.players[0].image){
+						$scope.winner = "O WIN!";
+					}
+					else if ($scope.xTurn == $scope.players[1].image){
+						$scope.winner = "X WIN!";;
+					};
+
+
+					// document.getElementsByClassName("winner")[0].style.display="block";
+					// $scope.winner = "You WIN!";
+					// break;
 				}
 			// else {
 			// 		alert("im checking winAry position:  "+ i)
